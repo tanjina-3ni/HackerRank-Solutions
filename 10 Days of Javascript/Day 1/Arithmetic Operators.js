8b3d18c74cac7a28dcaf02e4c7197254d8115207
Tanjina Rahman <tanjina.3ni@gmail.com>
2021-06-27 23:58:32 +0600

    Day1

---
+++ 10 Days of Javascript/Day 1/Arithmetic Operators.js
@@ -0,0 +1,14 @@
+/**
+*   Calculate the area of a rectangle.
+*
+*   length: The length of the rectangle.
+*   width: The width of the rectangle.
+*   
+*	Return a number denoting the rectangle's area.
+**/
+function getArea(length, width) {
+    let area;
+    // Write your code here
+    area = length*width;
+    return area;
+}
