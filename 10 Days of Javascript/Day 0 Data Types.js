12fdf4a18d9376cc919e44a53b6b2573c2fb898c
Tanjina Rahman <tanjina.3ni@gmail.com>
2021-06-27 23:48:33 +0600

    HackerRank

---
+++ 10 Days of Javascript/Day 0 Data Types.js
@@ -0,0 +1,22 @@
+function performOperation(secondInteger, secondDecimal, secondString) {
+    // Declare a variable named 'firstInteger' and initialize with integer value 4.
+    const firstInteger = 4;
+    var x = Number(secondInteger);
+    console.log(x + firstInteger)
+    
+    // Declare a variable named 'firstDecimal' and initialize with floating-point value 4.0.
+    const firstDecimal = 4.0;
+    secondDecimal = Number(secondDecimal);
+    console.log(firstDecimal+secondDecimal);
+    // Declare a variable named 'firstString' and initialize with the string "HackerRank".
+    const firstString = 'HackerRank ';
+    console.log(firstString + secondString);
+    // Write code that uses console.log to print the sum of the 'firstInteger' and 'secondInteger' (converted to a Number        type) on a new line.
+    
+    
+    // Write code that uses console.log to print the sum of 'firstDecimal' and 'secondDecimal' (converted to a Number            type) on a new line.
+    
+    
+    // Write code that uses console.log to print the concatenation of 'firstString' and 'secondString' on a new line. The        variable 'firstString' must be printed first.
+    
+}
