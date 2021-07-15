86f86a6cfed45f87665fc83d689510586368d38a
Tanjina Rahman <tanjina.3ni@gmail.com>
2021-07-15 22:09:34 +0600

    Day 17

---
+++ 30 Days of Code/PHP/Day 17 - More Exceptions.php
@@ -0,0 +1,24 @@
+<?php
+//Enter your code here
+class Calculator{
+    function __construct(){}
+    function power($n,$p){
+        if ($n < 0 || $p < 0)
+            throw new Exception("n and p should be non-negative");
+        return pow($n,$p);
+    }
+}
+
+$myCalculator=new Calculator();
+$T=intval(fgets(STDIN));
+while($T-->0){
+    list($n, $p)  = explode(" ", trim(fgets(STDIN)));
+    try{
+        $ans=$myCalculator->power($n,$p);
+        echo $ans."\n";
+    }
+    catch(Exception $e){
+        echo $e->getMessage()."\n";
+    }
+}
+?>
